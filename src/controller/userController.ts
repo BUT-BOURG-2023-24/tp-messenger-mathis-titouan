8eca33a70c82750f5a1c95891043f1c701982c7e
GERBOUD MATHIS p2104597 <mathis.gerboud@etu.univ-lyon1.fr>
2023-11-15 08:20:49 +0100

    Update userController.ts

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -17,7 +17,72 @@ class UserController {
         }
     }
 
-    
+    public async getAllUsers() {
+        try {
+            const users = await User.find();
+            return users;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    public async getUserByUsername(username : string) {
+        try {
+            const user = await User.findOne({ username });
+            return user;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    public async getUserById(userId : string) {
+        try {
+            const user = await User.findById(userId);
+            return user;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    public async getUsersByIds(userIds : string[]) {
+        try {
+            const users = await User.find({ _id: { $in: userIds } });
+            return users;
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
+
+    public async login(username : string, password : string) {
+        try {
+            let user = await User.findOne({ username });
+            if (!user) {
+                let hashedPassword = await bcrypt.hash(password, 10);
+                user = new User({
+                    username: username,
+                    password: hashedPassword,
+                    profilePicId: pickRandom()
+                });
+                await user.save();
+            } else {
+                const isPasswordCorrect = await bcrypt.compare(password, user.password.toString());
+                if (!isPasswordCorrect) {
+                    return { code: 400, error: 'Wrong password' };
+                }
+            }
+
+            const token = jwt.sign({ id: user._id }, config.JWT_SECRET);
+
+            return { user, token };
+        } catch (error) {
+            console.error(error);
+            return error;
+        }
+    }
 }
 
 
