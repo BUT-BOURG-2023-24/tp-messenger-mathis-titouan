805fd91eb3ca0f1a2001975582c07fcecda84b2d
Titouan <inviousit@gmail.com>
2023-12-10 22:51:26 +0100

    user test

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -18,18 +18,62 @@ describe('USERS', () =>
 	});
 
 	test("Login unexisting user", async () => {
+		const username = "nonexistentUser";
+		const password = "nonexistentPassword";
 
+		const response = await supertest(app)
+			.post("/users/login")
+			.send({ username, password });
+
+		expect(response.status).toBe(200);
+
+		expect(response.body.user).toBeDefined();
+		expect(response.body.token).toBeDefined();
+
+		expect(response.body.user.username).toBe(username);
+
+		expect(response.body.isNewUser).toBe(true);
 	});
 
 	test("Login existing user", async () => {
+		const username = "test";
+		const password = "testpwd";
 
+		const response = await supertest(app)
+			.post("/users/login")
+			.send({ username, password });
+
+		expect(response.status).toBe(200);
+
+		expect(response.body.user).toBeDefined();
+		expect(response.body.token).toBeDefined();
+
+		expect(response.body.user.username).toBe(username);
+
+		expect(response.body.isNewUser).toBe(false);
 	});
 
 	test("Login wrong password", async () => {
+		const username = "test";
+		const wrongPassword = "wrongPassword";
 
+		const response = await supertest(app)
+			.post("/users/login")
+			.send({ username, wrongPassword });
+
+		expect(response.status).toBe(400);
+
+		expect(response.body.error).toBe("Wrong password");
 	});
 
 	test("GET active users", async () => {
+		const response = await supertest(app)
+			.get("/users/online")
+			.set('Authorization', `Bearer ${token}`);
 
+		expect(response.status).toBe(200);
+
+		expect(response.body.users).toBeDefined();
+		expect(response.body.users.length).toBeGreaterThan(0);
 	});
 });
